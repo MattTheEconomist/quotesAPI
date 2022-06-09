264a97e2e0ae75c1fd271a90680f005ce2a685e4
Matt Ashford <mattashford1@gmail.com>
2022-06-09 09:24:19 -0400

    finished

--- server.js
+++ server.js
@@ -4,8 +4,40 @@ const app = express();
 const { quotes } = require("./data");
 const { getRandomElement } = require("./utils");
 
-const PORT = process.env.PORT || 4001;
+quoteRouter = express.Router();
+app.use("/api/quotes", quoteRouter);
+
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static("public"));
 
-console.log("yoo");
+app.listen(PORT, () => {
+  console.log("app is listening on port " + PORT);
+});
+
+// app.get("/api/quotes/random", (req, res, next) => {
+quoteRouter.get("/random", (req, res, next) => {
+  const quote = getRandomElement(quotes);
+  res.send({ quote: quote });
+});
+
+quoteRouter.get("/", (req, res, next) => {
+  if (!req.query.hasOwnProperty("person")) {
+    res.send({ quotes: quotes });
+  } else {
+    const filterQuote = quotes.filter(
+      (element) => element.person === req.query.person
+    );
+    res.send({ quotes: filterQuote });
+  }
+});
+
+quoteRouter.post("/", (req, res, next) => {
+  if (req.query.quote && req.query.person) {
+    const newQuote = { quote: req.query.quote, person: req.query.person };
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400).send();
+  }
+});
